b1b49dc5ea8481b20fa2ebb28f8e608b1189875a
jpflorido <jpflorido@gmail.com>
2014-11-27 20:22:32 +0100

    test

--- test_calculator.py
+++ test_calculator.py
@@ -1,14 +1,14 @@
 from unittest import TestCase
 from Calculator import *
-from mockito import *
+#from mockito import *
 __author__ = 'javier'
 
 
 class TestCalculator(TestCase):
     def test_suma(self):
-        #my_calc = Calculator(2, 4)
-        #self.assertEqual(my_calc.suma(), 6)
-        my_calc=mock(Calculator)
-        when(my_calc).suma().thenReturn(6)
-        self.assertEqual(my_calc.suma(),9)
-        a=2
+        my_calc = Calculator(2, 4)
+        self.assertEqual(my_calc.suma(), 6)
+        # my_calc=mock(Calculator)
+        # when(my_calc).suma().thenReturn(6)
+        # self.assertEqual(my_calc.suma(),9)
+        # a=2
